5775b766a08c34c4a680f1d7c62a18e0fff8655e
Edi Javier <126507385+edijavier99@users.noreply.github.com>
2023-06-08 19:25:19 +0000

    chnages

--- src/js/component/traficLight.jsx
+++ src/js/component/traficLight.jsx
@@ -1,45 +1,25 @@
 import React from "react";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 let TrafficLight = () =>{
-    const [redOn, setRedOn] = useState(false);
-    const [yellowOn, setYellowOn] = useState(false);
-    const [greenOn, setGreenOn] = useState(false)
-  
-    const redClick = () => {
-        setRedOn(true);
-        setYellowOn(false);
-        setGreenOn(false)
-
-    };
-  
-    const yellowClick = () => {
-        setRedOn(false);
-        setYellowOn(true);
-        setGreenOn(false)
-
-    };
-
-    const greenClick = () =>{
-        setGreenOn(true)
-        setRedOn(false);
-        setYellowOn(false);
-    }
+    const [color, setColor] = useState("");  
     return (    
         <div className=" d-flex justify-content-center align-items-center vh-100 flex-column" >
             <div className="stick d-block"></div>
             <div className="row d-flex justify-content-center  align-items-center container">
-                <div
-                    className={`mb-1 red ${redOn ? "red-on" : ""}`} onClick={redClick}>
-                </div>
-                <div
-                    className={`mb-1 yellow ${yellowOn ? "yellow-on" : ""}`} onClick={yellowClick}>
-                </div>
-                <div className={`mb-1 green ${greenOn ? "green-on" : ""}`} onClick={greenClick} >
-                </div>
+                <div className={"red " + (color==="red" ? "selected" : " ")} onClick={()=>{
+                    setColor("red")
+                }}></div> 
+                 <div className={"yellow " + (color==="yellow" ? "selected" : " ")} onClick={()=>{
+                    setColor("yellow")
+                }}></div> 
+                 <div className={"green " + (color==="green" ? "selected" : " ")} onClick={()=>{
+                    setColor("green")
+                }}></div> 
+            </div>
+            <div>
             </div>
         </div>
     )
 }
-
 export default TrafficLight;
